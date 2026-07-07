7b7558885e9554774e131a90fb7b46be1194a5f4
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-07-07 16:52:26 +0700

    add database module, prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProductModule, AuthModule, UserModule],
+  imports: [ProductModule, AuthModule, UserModule, DatabaseModule],
 })
 export class AppModule {}
